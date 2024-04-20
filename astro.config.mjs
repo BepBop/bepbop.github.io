dc8a7d40bfc0c90468dff1f2db22308b11fdd821
BepBop <33974452+BepBop@users.noreply.github.com>
2024-04-20 15:14:35 +0530

    Changed Absolute Path To Relative

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import {defineConfig} from 'astro/config';
 
 import react from "@astrojs/react";
-import {current_year} from "/Users/gyananjay/Documents/Webstorm/https-bepbop.github.io/src/components/Timeline.jsx";
+import {current_year} from "./src/components/Timeline.jsx";
 
 // https://astro.build/config
 export default defineConfig({
