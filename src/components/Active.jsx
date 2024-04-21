dfe770176673b84a83e9565dc534941ae2ca1c5d
BepBop <33974452+BepBop@users.noreply.github.com>
2024-04-21 10:49:27 +0530

    Rewrote the shtty hack to fix the path issue

--- src/components/Active.jsx
+++ src/components/Active.jsx
@@ -6,7 +6,7 @@ export default function ({year}) {
     return (
         <div className={style.test}>
             <div className={style.selected + ' ' + style.active}>
-                <div className={style.subheader}>Selected</div>
+                <div className={style.subheader}></div>
                 <div className={style.year}>{year}</div>
             </div>
 
