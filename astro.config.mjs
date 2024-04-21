e2180634138e97cc8d34665865e70bf7c93e6659
BepBop <33974452+BepBop@users.noreply.github.com>
2024-04-21 10:37:38 +0530

    Rewrote the shtty hack to fix the path issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,5 @@ import {current_year} from "./src/layouts/Timeline.jsx";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [react()],
-    redirects:{
-        '/{current_year}': '/',
-        '/2023':'/'
-    }
+    integrations: [react()]
 });
